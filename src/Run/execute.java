36dce3c135345e8377f6300222c23532901b3895
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-08-14 10:21:48 +0530

    yt

--- src/Run/execute.java
+++ src/Run/execute.java
@@ -5,6 +5,8 @@ import static Package.greetings.message;
 
 public class execute {
     public static void main(String[] args) {
+
+        System.out.println("I am from the package greetings.message");
         message();
     }
 }
