2cbdc5352f86730919af0fe149e9e49e3b502116
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-08-14 10:20:53 +0530

    yt

---
+++ src/Package/greetings.java
@@ -0,0 +1,10 @@
+package Package;
+
+public class greetings {
+    public static void main(String[] args) {
+        System.out.println("Hello This Name is Jijo");
+    }
+    public static void message(){
+        System.out.println("From package package directory");
+    }
+}
---
+++ src/Run/execute.java
@@ -0,0 +1,10 @@
+package Run;
+
+//import static Package.Messages.message;
+import static Package.greetings.message;
+
+public class execute {
+    public static void main(String[] args) {
+        message();
+    }
+}
